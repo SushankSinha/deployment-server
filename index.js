b1428902af6f51be9bfd255a2a39fe47e4f2e36a
SushankSinha <SushankSinha>
2023-09-03 13:04:38 +0530

    console update

--- index.js
+++ index.js
@@ -15,45 +15,6 @@ mongoose.connect(MONGO_URL, {useUnifiedTopology : true,
   console.log ("Mongoose connection started")
 }).catch((err)=> console.log("Mongoose connection refused", err))
 
-//get all books
-// app.get("/books", async (req, res) => {
-//   const books = await client
-//     .db("b47-book-app")
-//     .collection("books")
-//     .find()
-//     .toArray();
-//   res.send(books);
-// });
-
-// //delete book by ID
-// app.delete("/books/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const book = await client
-//     .db("b47-book-app")
-//     .collection("books")
-//     .deleteOne({ id: id });
-//   res.send(book);
-// });
-
-// app.post("/books", async (req, res) => {
-//   const newBooks = req.body;
-//   const output = await client
-//     .db("b47-book-app")
-//     .collection("books")
-//     .insertMany(newBooks);
-//   res.send(output);
-// });
-
-// app.put("/books/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const updatedBooks = req.body;
-//   const output = await client
-//     .db("b47-book-app")
-//     .collection("books")
-//     .updateOne({ id: id }, { $set: updatedBooks });
-//   res.send(output);
-// });
-
 app.listen(PORT, () => console.log("Server starting on port", PORT));
 
 app.get("/", function (req, res) {
